ebbbb06aadb8b7a0c5ab50d6c8672d0148821dd1
VictorYJM <vymimura2@gmail.com>
2026-08-18 16:08:39 -0300

    Small changes for updates

--- src/main/java/devtools/GenerateDevKeystore.java
+++ src/main/java/devtools/GenerateDevKeystore.java
@@ -2,6 +2,8 @@ package devtools;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
@@ -13,10 +15,10 @@ public class GenerateDevKeystore {
 
         char[] password = "changeit".toCharArray();
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
-        keyStore.load(null, password); // null = cria um keystore vazio novo
+        keyStore.load(null, password);
 
-        SecretKeySpec privSpec = new SecretKeySpec(pair.getPrivate().getEncoded(), "RAW");
-        SecretKeySpec pubSpec = new SecretKeySpec(pair.getPublic().getEncoded(), "RAW");
+        SecretKeySpec privSpec = new SecretKeySpec(pair.getPrivate().getEncoded(), "X25519");
+        SecretKeySpec pubSpec = new SecretKeySpec(pair.getPublic().getEncoded(), "X25519");
 
         keyStore.setEntry("project-key-v1-priv",
                 new KeyStore.SecretKeyEntry(privSpec),
@@ -25,9 +27,18 @@ public class GenerateDevKeystore {
                 new KeyStore.SecretKeyEntry(pubSpec),
                 new KeyStore.PasswordProtection(password));
 
-        try (var out = new java.io.FileOutputStream("src/main/resources/keystore/dev-keystore.p12")) {
+        Path keystorePath = Path.of(
+                "src/main/resources/keystore/dev-keystore.p12"
+        );
+
+        Files.createDirectories(keystorePath.getParent());
+
+        try (var out = Files.newOutputStream(keystorePath)) {
             keyStore.store(out, password);
         }
-        System.out.println("Keystore gerado com sucesso.");
+
+        System.out.println(
+                "Keystore gerado com sucesso em: " + keystorePath.toAbsolutePath()
+        );
     }
 }
